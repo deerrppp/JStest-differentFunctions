4bcedc6bdceb7f50f37da9a32d40cae750d97b9c
deerrppp <deerrppp@gmail.com>
2023-10-23 22:11:21 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const textBox = document.querySelector("#textBox")
+const output = document.querySelector("#output");
+
+// ----ANONYMOUS FUNCTION---- 
+function logKey(event) {
+    output.textContent = `You pressed "${event.key}".`;
+}
+
+ textBox.addEventListener("keydown", logKey);
